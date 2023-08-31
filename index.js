f5f80d081032cb1b5933d86fefce7862abea2340
PauTG <paulit.codes@gmail.com>
2023-08-31 19:30:33 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,61 @@
+const timerDisplay = document.getElementById('timer');
+const startButton = document.getElementById('start');
+const resetButton = document.getElementById('reset');
+
+let timeLeft = 1500;
+let isRunning = false;
+let timerInterval;
+let pomodoroCount = 0;
+
+function updateTimer() {
+    const minutes = Math.floor(timeLeft / 60);
+    const seconds = timeLeft % 60;
+    timerDisplay.textContent = `${minutes}:${seconds.toString().padStart(2, '0')}`;
+}
+
+function startTimer() {
+    if (!isRunning) {
+        isRunning = true;
+        startButton.textContent = 'Pause';
+        timerInterval = setInterval(() => { 
+            if (timeLeft < 1){
+                document.getElementById("notification").play();
+            }
+            if (timeLeft > 0) {
+                timeLeft--;
+                updateTimer();
+            } else {
+                clearInterval(timerInterval);
+                isRunning = false;
+                startButton.textContent = 'Start';
+                if (pomodoroCount % 2 === 0) {
+                    timeLeft = 1500; 
+                } else {
+                    timeLeft = 1500; 
+                }
+                pomodoroCount++;
+                updateTimer();
+            }
+        }, 1000);
+    } else {
+        clearInterval(timerInterval);
+        isRunning = false;
+        startButton.textContent = 'Resume';
+    }
+}
+
+function resetTimer() {
+    clearInterval(timerInterval);
+    isRunning = false;
+    startButton.textContent = 'Start';
+    timeLeft = 1500;
+    pomodoroCount = 0;
+    updateTimer();
+}
+
+
+startButton.addEventListener('click', startTimer);
+resetButton.addEventListener('click', resetTimer);
+
+updateTimer();
+
